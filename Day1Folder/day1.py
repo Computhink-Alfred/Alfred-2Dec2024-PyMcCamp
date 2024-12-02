7595383ee39097ead2d088dacabe7989db4b6ca1
Computhink-Alfred <167395775+Computhink-Alfred@users.noreply.github.com>
2024-12-02 04:29:25 +0000

    Day 1

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,4 +1,44 @@
 # Write all your codes for Day 1 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day1")
+
+# Printing self introduction
+# print("hello from day1")
+# print("My name is Alfred")
+# print("I am teaching Python Minecraft")
+
+# animal1 = "Zebra"
+# animal2 = "Tiger"
+# animal3 = "griaffe"
+
+# print(animal1)
+# print(animal2)
+# print(animal3)
+
+# num1 = 5
+# num2 = 10
+# ans = num1 + num2
+
+# print(ans)
+
+# food = "fish"
+# animal = "cat"
+
+# print("I fed my " + animal + " some " + food)
+
+# def intro(name, age):
+#     print("My name is " + name)
+#     print("I am " + age + " this year")
+#     print("I am currently teaching Python")
+
+# intro("John", "20")
+# intro("James", "25")
+
+
+
+def sumOf(num1, num2):
+    print(num1 + num2)
+
+sumOf(10, 20)
+sumOf(5, 20)
+sumOf(10, 10)
