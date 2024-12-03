2df6183d9daec5a73ee5710c56a6a45144852981
Computhink-Alfred <167395775+Computhink-Alfred@users.noreply.github.com>
2024-12-03 04:33:31 +0000

    Day 2

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -1,4 +1,31 @@
 # Write all your codes for Day 2 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day2")
+# for i in "KDHJASFKLASHFDLK":
+#     print(i)
+#     print("This is 1 repeat")
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(10):
+#     print("This is a sentence I want to repeat")
+
+# counter = 1
+
+# for i in "ALFRED":
+#     print(counter)
+#     counter = counter + 1
+
+# name = "JONATHAN"
+
+# for i in name:
+#     print('Give me a "' + i + '"!')
+
+# print()
+# print("WHO IS THE BEST!!!?!?!?!?")
+# print()
+# print(name + "!!!!!!!!")
+
+for i in range(2):
+    print(i)
