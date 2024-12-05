0f009ea0e223afe97f304d5b98f5f5b0bebdc748
Computhink-Alfred <167395775+Computhink-Alfred@users.noreply.github.com>
2024-12-05 03:10:34 +0000

    Day 4

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -1,4 +1,35 @@
 # Write all your codes for Day 4 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day4")
+# print("hello from day4")
+
+# password = input("Please enter your password: ")
+# counter = 1
+
+# while counter < 5:
+#     if password != "Alfred":
+#         print("You have tried " + str(counter) + " times." )
+#         password = input("Try again: ")
+#         counter = counter + 1
+#     else:
+#         break
+
+# if counter >= 5:
+#     print("You are locked out")
+# else:
+#     print("You have successfully logged in")
+
+userAns = input("What is opposite of black? ")
+counter = 1
+
+while userAns != "white":
+    if counter == 2:
+        print("Answer starts with 'w'")
+    elif counter == 4:
+        print("Answer is 5 letters long")
+    elif counter > 6:
+        print("It is a lack of colour")
+    userAns = input("What is opposite of black? ")
+    counter = counter + 1
+
+print("You are correct!")
